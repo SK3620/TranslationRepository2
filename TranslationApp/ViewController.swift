44a1381d351e47a5d4964f455bd4efd144ba9d66
kenta.suzuki <kenta.suzuki@techacademy.jp>
2022-08-27 00:14:19 +0900

    Initial Commit

---
+++ TranslationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TranslationApp
+//
+//  Created by 鈴木健太 on 2022/08/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
